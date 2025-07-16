b6751f77c3b0a8dbe1d3c8f8a092401fe825d2de
Arnius <arnasg08@gmail.com>
2025-07-16 19:55:38 +0300

    Plugin batch update

--- plugin/gr/.conda/recipe/bld.bat
+++ plugin/gr/.conda/recipe/bld.bat
@@ -10,7 +10,7 @@ dir ..
 cmake -G "Ninja" ^
     -DCMAKE_BUILD_TYPE=Release ^
     -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ..
+    -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ../plugin/gr
 
 if errorlevel 1 exit 1
 
